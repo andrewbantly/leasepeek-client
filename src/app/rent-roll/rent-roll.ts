ca9c323d50b28dbdb09687b53c164f59bc37f1cb
andrewbantly <andrewbantly@gmail.com>
2023-07-25 16:11:35 -0700

    Unit interface

---
+++ src/app/rent-roll/rent-roll.ts
@@ -0,0 +1,21 @@
+export interface Unit {
+    status: string;
+    unit: number;
+    unitType: string;
+    unitSqFt: number;
+    resident: number;
+    name: string;
+    marketRent: number;
+    val: number;
+    resDeposit: number;
+    otherDeposit: number;
+    moveIn: Date;
+    leaseExp: Date;
+    moveOut: Date;
+    balance: number;
+    rliab: number;
+    amenf: number;
+    car: number;
+    rntres: number;
+    total: number;
+}
