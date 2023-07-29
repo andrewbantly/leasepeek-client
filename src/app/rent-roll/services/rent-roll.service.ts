e4c24765045a5332d10a656c1a23effb2da0cf2b
andrewbantly <andrewbantly@gmail.com>
2023-07-29 13:27:10 -0700

    successful test POST to backend and DB

--- src/app/rent-roll/services/rent-roll.service.ts
+++ src/app/rent-roll/services/rent-roll.service.ts
@@ -2,20 +2,22 @@ import { Injectable, Inject } from '@angular/core';
 import { APP_SERVICE_CONFIG } from 'src/app/AppConfig/appconfig.service';
 import { Appconfig } from 'src/app/AppConfig/appconfig';
 import { Unit } from '../rent-roll';
-import { HttpClient, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpHeaders, HttpEventType, HttpResponse, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs'; // Import the Observable type
 import { environment } from 'src/environments/environemnt';
+import { filter, map } from 'rxjs/operators'; // Import filter and map operators
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RentRollService {
 
-  constructor(@Inject(APP_SERVICE_CONFIG) private config: Appconfig, private http: HttpClient) { 
+  constructor(@Inject(APP_SERVICE_CONFIG) private config: Appconfig, private http: HttpClient) {
     console.log('api end point:');
     console.log(this.config.apiEndPoint)
     console.log(environment.apiEndpoint)
-   }
+  }
 
   getRentRoll(): Observable<any> {
     const request = new HttpRequest('GET', `http://127.0.0.1:8000/`, {
@@ -25,16 +27,23 @@ export class RentRollService {
   };
 
   upload(formData: FormData): Observable<any> {
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer YOUR_ACCESS_TOKEN', // Replace YOUR_ACCESS_TOKEN with your actual token if needed
-      'Content-Type': 'application/json' // Assuming you are sending JSON data
-    });
-    return this.http.post('http://127.0.0.1:8000/upload', formData, { headers: headers });
+    // const headers = new HttpHeaders({
+    //   'Authorization': 'Bearer YOUR_ACCESS_TOKEN', // Replace YOUR_ACCESS_TOKEN with your actual token if needed
+    //   'Content-Type': 'application/json' // Assuming you are sending JSON data
+    // });
+    let human: Object = {
+      name: "Kate",
+      age: "28",
+      created: false
+    }
+    return this.http.post('http://127.0.0.1:8000/api/test/', human);
+    // return this.http.post('http://127.0.0.1:8000/upload/', formData, { headers: headers });
   }
-  
-
 
 
 }
+
+
+
 
 
--- src/app/request.interceptor.ts
+++ src/app/request.interceptor.ts
@@ -15,21 +15,28 @@ export class RequestInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     console.log('Request Interceptor', request);
-    
-    if (request.method === "POST") {
-      const token = 'YOUR_ACCESS_TOKEN'; // Replace this with your actual token value or get it from a service/storage
 
-      // Clone the request and set the "token" header
-      const newRequest = request.clone({
-        setHeaders: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json' // Assuming you are sending JSON data
-        }
+    if (request.method === "POST") {
+      // Get the CSRF token from the cookie (name: 'csrftoken')
+      const csrfToken = this.getCookie('csrftoken');
+
+      // Set the CSRF token in the request headers
+      const headers = new HttpHeaders({
+        'X-CSRFToken': csrfToken!
       });
 
-      return next.handle(newRequest);
+      // Clone the request with the updated headers
+      request = request.clone({ headers });
     }
 
     return next.handle(request);
+  }
+
+  // Helper function to get the value of a cookie
+  private getCookie(name: string): string | null {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
+    return null;
   }
 }
