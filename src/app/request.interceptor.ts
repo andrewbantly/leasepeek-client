bb16ae67ff7c52f57596ecc2e191b5463426734a
andrewbantly <andrewbantly@gmail.com>
2023-07-29 14:26:20 -0700

    request interceptor cleanup

--- src/app/request.interceptor.ts
+++ src/app/request.interceptor.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpHeaders
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -15,28 +9,7 @@ export class RequestInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     console.log('Request Interceptor', request);
-
-    if (request.method === "POST") {
-      // Get the CSRF token from the cookie (name: 'csrftoken')
-      const csrfToken = this.getCookie('csrftoken');
-
-      // Set the CSRF token in the request headers
-      const headers = new HttpHeaders({
-        'X-CSRFToken': csrfToken!
-      });
-
-      // Clone the request with the updated headers
-      request = request.clone({ headers });
-    }
-
     return next.handle(request);
   }
 
-  // Helper function to get the value of a cookie
-  private getCookie(name: string): string | null {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
-    return null;
-  }
 }
