a1a6553176415c0c969d2ff5447e64a5c84487f0
andrewbantly <andrewbantly@gmail.com>
2023-08-01 19:40:46 -0700

    login form button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
@@ -9,7 +10,7 @@ import {FormsModule} from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
 export class LoginComponent {
 
