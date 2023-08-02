5669fee19d5a31cd01fbc97d1e47176f63b335b7
andrewbantly <andrewbantly@gmail.com>
2023-08-01 21:18:40 -0700

    building post call to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-
+import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginService } from './login.service';
+import { Login } from './login';
 
 @Component({
   selector: 'rroll-login',
@@ -13,5 +14,21 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
 export class LoginComponent {
+  user : Login = {
+    username: "",
+    password: "",
+  }
+  message : string = "";
+
+  constructor(private loginService: LoginService) { }
+
+  login(loginForm: NgForm) {
+    console.log("Login form:")
+    console.log(loginForm)
+    this.loginService.login(this.user).subscribe((data) => {
+      console.log("LOGIN DATA")
+      console.log(data)
+    })
+  }
 
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { Login } from './login';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environemnt';
+import { Appconfig } from '../AppConfig/appconfig';
+import { APP_SERVICE_CONFIG } from '../AppConfig/appconfig.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor() { }
+  // userLogin : Login = {
+  //   username: "",
+  //   password: ""
+  // }
+  constructor(@Inject(APP_SERVICE_CONFIG) private config: Appconfig, private http: HttpClient) { 
+      console.log('api end point:');
+      console.log(environment.apiEndpoint)
+    }
+    login(user: Login) {
+      return this.http.post<Login>('/api/login', user)
+    }
 }
