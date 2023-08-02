058f823c53967a2c3486962eb4c7429daa0dc4d0
andrewbantly <andrewbantly@gmail.com>
2023-08-01 19:09:27 -0700

    router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  {path: '', component: LoginComponent},
   {path: 'profile', component: UserComponent},
   { path: '**', component: NotfoundComponent },
 ];
