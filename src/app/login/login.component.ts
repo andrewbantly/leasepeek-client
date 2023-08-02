b08cd2c022949c60c1bbd6e64a7f5e9f0557a187
andrewbantly <andrewbantly@gmail.com>
2023-08-01 19:31:43 -0700

    login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,10 +3,13 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 
+
 @Component({
   selector: 'rroll-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
 })
 export class LoginComponent {
 
