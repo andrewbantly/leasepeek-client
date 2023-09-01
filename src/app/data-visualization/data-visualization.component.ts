7734341c9aefddda4c32bbe27f46bb7334306a4b
andrewbantly <andrewbantly@gmail.com>
2023-08-31 19:38:39 -0700

    data is rendering on the screen, sorted

--- src/app/data-visualization/data-visualization.component.ts
+++ src/app/data-visualization/data-visualization.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../api/api.service';
-import { HttpClient } from '@angular/common/http';
+// import * as d3 from 'd3';
+import { MarketRentXDate } from './data-visualization';
 
 @Component({
   selector: 'rroll-data-visualization',
@@ -11,23 +12,55 @@ import { HttpClient } from '@angular/common/http';
 export class DataVisualizationComponent implements OnInit {
 
   objectId: string | null = "";
+  building: string = "";
+  asOfDate: string = "";
   message: string = "";
+  rentData: any[] = [];
+  @Input() marketRentxDate: MarketRentXDate[] = [];
+  
 
-
-  constructor(private route: ActivatedRoute, private apiService: ApiService, private http:HttpClient) {}
+  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.objectId = this.route.snapshot.paramMap.get('objectId');
     if (this.objectId) {
-      this.message = "Loading data"
+      this.message = "Loading data";
       this.apiService.loadExcelData(this.objectId).subscribe(
         dataLoadResponse => {
-          console.log("Data load response:", dataLoadResponse)
+          console.log("Data load response:", dataLoadResponse);
+          this.building = dataLoadResponse[0].building;
+          this.asOfDate = dataLoadResponse[0].date;
+          this.rentData = dataLoadResponse[0].data;
+          console.log("true or false", this.rentData)
+          this.gatherRentxData(this.rentData)
+          this.message = ""
         }
       )
     } else {
-      console.error("objectId is missing or null")
-      this.message = "Error loading data"
+      console.error("objectId is missing or null");
+      this.message = "Error loading data";
     }
   }
+
+
+  gatherRentxData(data: any[]): void {
+    data.forEach(unit => {
+        if (unit.moveIn && unit.marketRent) {
+            const moveInDate = unit.moveIn;
+            const rent = unit.marketRent;
+            this.marketRentxDate.push({
+                date: moveInDate,
+                marketRent: rent
+            });
+        }
+    });
+    this.marketRentxDate.sort((a, b) => {
+      if (a.date < b.date) return -1;
+      if (a.date > b.date) return 1;
+      return 0;
+  });
+  
+}
+
+
 }
---
+++ src/app/data-visualization/data-visualization.ts
@@ -0,0 +1,4 @@
+export interface MarketRentXDate {
+    date: string;
+    marketRent: number;
+}
