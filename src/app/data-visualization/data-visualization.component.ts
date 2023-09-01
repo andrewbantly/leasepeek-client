8315ab8677c19e85086f6ba27ce2b2e4df79dae0
andrewbantly <andrewbantly@gmail.com>
2023-08-31 18:02:47 -0700

    routing based on objectId working

--- src/app/data-visualization/data-visualization.component.ts
+++ src/app/data-visualization/data-visualization.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'rroll-data-visualization',
   templateUrl: './data-visualization.component.html',
   styleUrls: ['./data-visualization.component.scss']
 })
-export class DataVisualizationComponent {
+export class DataVisualizationComponent implements OnInit {
+
+  objectId: string | null = "";
+
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.objectId = this.route.snapshot.paramMap.get('objectId');
+  }
 
 }
